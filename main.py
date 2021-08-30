f0257530fc3cf627992f9dced948e24942751637
woongcheol <1994_100@naver.com>
2021-08-31 01:15:52 +0900

    change

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def         helloworld(a):
-    print(f"Hello World! {a}!")#adasdasd
+def helloworld(a):
+    print(f"Hello World! {a}!")  # adasdasd
 
 
 if __name__ == "__main__":
     helloworld("Chris")
-
